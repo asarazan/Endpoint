206733646a5dd3dbfe77e01aebd3bd6a151089c3
Aaron Sarazan <aaron@levelmoney.com>
2014-08-28 13:52:24 -0700

    Broke endpoint logic into its own repo

---
+++ parent/endpoint/src/main/java/net/sarazan/endpoint/factory/Responses.java
@@ -0,0 +1,34 @@
+package net.sarazan.endpoint.factory;
+
+import net.sarazan.endpoint.i.Response;
+import net.sarazan.endpoint.impl.response.BasicResponse;
+import net.sarazan.endpoint.impl.response.FailedResponse;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by Aaron Sarazan on 5/13/14
+ * Copyright(c) 2014 Manotaur, LLC.
+ */
+public final class Responses {
+
+    private static final String TAG = "Responses";
+
+    private Responses() {}
+
+    public static <T> Response<T> failedResponse(Exception cause) {
+        return new FailedResponse<>(cause);
+    }
+
+    public static <T> Response<T> failedResponse(int code) {
+        return new FailedResponse<>(code);
+    }
+
+    public static <T> Response<T> failedResponse(Response<?> other) {
+        return new FailedResponse<>(other);
+    }
+
+    public static <T> Response<T> createResponse(@NotNull T data) {
+        return new BasicResponse<>(data);
+    }
+}
---
+++ parent/endpoint/src/main/java/net/sarazan/endpoint/i/Processor.java
@@ -0,0 +1,16 @@
+package net.sarazan.endpoint.i;
+
+import android.content.Context;
+
+import com.squareup.okhttp.Request.Builder;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by Aaron Sarazan on 5/18/14
+ * Copyright(c) 2014 Manotaur, LLC.
+ */
+public interface Processor {
+
+    void processRequest(@NotNull Context context, @NotNull Builder requestBuilder);
+}
